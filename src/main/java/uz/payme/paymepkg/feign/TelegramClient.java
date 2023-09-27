64b028c6595df5dd7dd8416da958f3f2933e33b8
Diyorbek <diyorbeknajmitdinov@gmail.com>
2023-09-28 01:19:45 +0500

    Before Deploy

--- src/main/java/uz/payme/paymepkg/feign/TelegramClient.java
+++ src/main/java/uz/payme/paymepkg/feign/TelegramClient.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import uz.payme.paymepkg.model.SuccessfulOrderResponse;
 
-@FeignClient(value = "telegram",url = "http://localhost:8000/api")
+@FeignClient(value = "telegram",url = "http://188.225.26.222:8000/api")
 public interface TelegramClient {
     @PostMapping("/success")
     void post(SuccessfulOrderResponse successfulOrderResponse);
